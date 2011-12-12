5464a00776f2611e76f74f7936bad192315563b5
lathomas <>
2011-12-12 17:34:37 +0000

    automatic selection of MC or data config

--- python/gsfcheckertree_cfi.py
+++ python/gsfcheckertree_cfi.py
@@ -4,9 +4,7 @@ gsfcheckerjob = cms.EDAnalyzer("GsfCheckerTree",
   src = cms.InputTag('genParticles'),
   logEvents = cms.uint32(10),                                        
   TriggerResultsTag = cms.InputTag("TriggerResults","","HLT"),
-#  TriggerResultsTag = cms.InputTag("TriggerResults","","RECO"),
-  usegendata = cms.bool(False),
-##  usegendata = cms.bool(True),
+  #TriggerResultsTag = cms.InputTag("TriggerResults","","RECO"),
 
   # for skimming on 2ele, 1ele-1mu and 2mu
   electronEtCut = cms.untracked.double(35.),
