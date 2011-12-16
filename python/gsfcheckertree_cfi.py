11897716dcb8d422c069a3548265eb550f17f273
treis <>
2011-12-16 13:52:52 +0000

    - changed parameters

--- python/gsfcheckertree_cfi.py
+++ python/gsfcheckertree_cfi.py
@@ -1,10 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 gsfcheckerjob = cms.EDAnalyzer("GsfCheckerTree",
-  src = cms.InputTag('genParticles'),
-  logEvents = cms.uint32(10),                                        
   TriggerResultsTag = cms.InputTag("TriggerResults","","HLT"),
   #TriggerResultsTag = cms.InputTag("TriggerResults","","RECO"),
+  centerOfMassEnergy = cms.double(7000.),
 
   # for skimming on 2ele, 1ele-1mu and 2mu
   electronEtCut = cms.untracked.double(35.),
