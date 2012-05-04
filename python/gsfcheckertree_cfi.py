06c2bd4fb4ef6667744bd43372ded26146b0ee82
lathomas <>
2012-05-04 16:17:39 +0000

    *** empty log message ***

--- python/gsfcheckertree_cfi.py
+++ python/gsfcheckertree_cfi.py
@@ -7,15 +7,31 @@ gsfcheckerjob = cms.EDAnalyzer("GsfCheckerTree",
   TriggerResultsTag = cms.InputTag("TriggerResults","","HLT"),
   #TriggerResultsTag = cms.InputTag("TriggerResults","","RECO"),
   centerOfMassEnergy = cms.double(8000.),
-  bJetPtMin = cms.untracked.double(10.),
 
+  #Pt cut for stored infos. This is *NOT* the skimming part ! 
+  bJetPtMin = cms.untracked.double(10.),
+  ScPtMin =  cms.untracked.double(10.),
+  GsfPtMin=   cms.untracked.double(10.),
+  GsfTrackPtMin=   cms.untracked.double(5.),
+                               
   # for skimming on 2ele, 1ele-1mu and 2mu
+<<<<<<< gsfcheckertree_cfi.py
+  electron1EtMin = cms.untracked.double(35),
+  electron1EtMax = cms.untracked.double(1.E99),
+  electron2EtMin = cms.untracked.double(35),
+  electron2EtMax = cms.untracked.double(1.E99),
+  muon1PtMin = cms.untracked.double(35),
+  muon1PtMax = cms.untracked.double(1.E99),                        
+  muon2PtMin = cms.untracked.double(35),
+  muon2PtMax = cms.untracked.double(1.E99),                              
+=======
   electron1EtMin = cms.untracked.double(35),
   electron1EtMax = cms.untracked.double(1.E99),
   electron2EtMin = cms.untracked.double(35),
   electron2EtMax = cms.untracked.double(1.E99),
   muon1PtMin = cms.untracked.double(35),
   muon2PtMin = cms.untracked.double(35),
+>>>>>>> 1.11
   IsoDepElectron = cms.VInputTag(cms.InputTag('elPFIsoDepositChargedPFIso'),
                    cms.InputTag('elPFIsoDepositGammaPFIso'),
                    cms.InputTag('elPFIsoDepositNeutralPFIso')),
